ebea79664b378cde7ef257843ce5c48bcea5620e
William L. Anderson <band@acm.org>
2023-07-29 14:56:02 -0500

    add boolean reindex argument

--- docSummaryPlus/llama-indexLab/oaiExpt02.py
+++ docSummaryPlus/llama-indexLab/oaiExpt02.py
@@ -1,6 +1,11 @@
 #! /usr/bin/env python
 """
 code derived from <https://gpt-index.readthedocs.io/en/latest/examples/vector_stores/SimpleIndexDemo.html>, accessed 2023-07-28
+
+two arguments:
+--wiki/-w: directory containing wiki files
+--model/-m: LLM model name
+--reindex/-r: build a new vector index; if omitted use the latest saved index
 """
 # Import necessary packages
 from llama_index import(
@@ -17,6 +22,7 @@ def init_argparse():
     parser = argparse.ArgumentParser(description='Generate OpenAI index from wiki text or Markdown files.')
     parser.add_argument('--wiki', '-w', required=True, help='directory containing wiki files (Markdown + other)')
     parser.add_argument('--model', '-m', required=False, help='llm model_name')
+    parser.add_argument('--reindex', '-r', action='store_true', help='build a new vector index')
     return parser
 
 def main():
@@ -40,25 +46,26 @@ def main():
     # Loading from a directory
     documents = SimpleDirectoryReader(wiki_dir).load_data()
 
-    # Construct a simple vector index
-    index = VectorStoreIndex.from_documents(documents)
+    # set storage context and load index
+    storage_context = StorageContext.from_defaults(persist_dir="./storage")
+    index = load_index_from_storage(storage_context, index_id="vector_index")
 
-    # Save index to disk
-    index.set_index_id("vector_index")
-    index.storage_context.persist("./storage")
+    if args.reindex:
+        # Construct a new simple vector index
+        index = VectorStoreIndex.from_documents(documents)
+        # and save index to disk
+        index.set_index_id("vector_index")
+        index.storage_context.persist("./storage")
 
-    # rebuild storage context
-#    storage_context = StorageContext.from_defaults(persist_dir="storage")
-    # load index
-#    index = load_index_from_storage(storage_context, index_id="vector_index")
-
-    # Querying the index
+    # Query the index
     query_engine = index.as_query_engine(
         service_context=service_context,
         response_mode="tree_summarize")
-    
-    response = query_engine.query("List the primary topics in the wiki pages")
+
+    question = "List the primary topics in the wiki pages"
+    response = query_engine.query(question)
     print('model name: ', model_name)
+    print('query: ', question)
     print(response)
 
 if __name__ == "__main__":
